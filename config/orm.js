0e539d7a06908cc8b9a718cf84eebf83b82557c6
seanianking <seanianking@gmail.com>
2020-04-11 22:38:01 -0600

    finished ORM configuration with the update and delete functions used in the burger.js file

--- config/orm.js
+++ config/orm.js
@@ -6,25 +6,43 @@ let connection = require('./connection');
 //3. updateOne()
 
 var orm = {
-    selectAll: function(cb){
+    selectAll: function (cb) {
         let queryString = "SELECT * FROM burgers;";
-        connection.query(queryString, function(err, result) {
-          if (err) {
-            throw err;
-          }
-          cb(result);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
         });
     },
-    create: function(burger_name, cb) {
+    create: function (burger_name, cb) {
         let queryString = "INSERT INTO burgers (burger_name) VALUES(?);";
         connection.query(queryString, [burger_name], (err, res) => {
-            if (err){
-                throw err;  
-            } 
+            if (err) {
+                throw err;
+            }
             cb(res);
         });
     },
-    
+
+    update: function (id, cb) {
+        let queryString = "UPDATE burgers SET chomped = 1  WHERE id = ?;";
+        connection.query(queryString, [id], (err, res) => {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    delete: function (id, cb) {
+        let queryString = "DELETE FROM burgers WHERE id = ?";
+        connection.query(queryString, [id], (err, res) => {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
 
 }
 
