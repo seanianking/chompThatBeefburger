79aa8cc850c110fed42abe2304524e4be68145d5
seanianking <seanianking@gmail.com>
2020-04-11 22:57:33 -0600

    add button clears input, but doesn't push information to database. eat it button also not working.

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+//Pulls needed modules
 const express = require("express");
+
+//Middleware
 const PORT = process.env.PORT || 8080;
 const app = express();
 
@@ -8,16 +11,16 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.json());
 
+//Sets handlebars up to be used
 const exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-
 app.set("view engine", "handlebars");
 
 const routes = require("./controllers/burgers_controller");
 
 app.use(routes);
 
+//Starts the app
 app.listen(PORT, function(){
     console.log("Server listening on http://localhost:" + PORT);
 })
