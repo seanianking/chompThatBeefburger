7e1002f4a1682a7f2b8631e7ae6fd2ebc05d8236
seanianking <seanianking@gmail.com>
2020-04-11 22:22:33 -0600

    added function calls to burger.js

--- config/orm.js
+++ config/orm.js
@@ -5,6 +5,29 @@ let connection = require('./connection');
 //2. insertOne()
 //3. updateOne()
 
+var orm = {
+    selectAll: function(cb){
+        let queryString = "SELECT * FROM burgers;";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+    },
+    create: function(burger_name, cb) {
+        let queryString = "INSERT INTO burgers (burger_name) VALUES(?);";
+        connection.query(queryString, [burger_name], (err, res) => {
+            if (err){
+                throw err;  
+            } 
+            cb(res);
+        });
+    },
+    
+
+}
+
 
 //export orm
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,20 @@
 let orm = require('../config/orm');
 
+let burger = {
+    selectAll: function(cb){
+        orm.selectAll(cb);
+    },
+    create: function(burger_name, cb){
+        orm.create(burger_name, cb);
+    },
+    update: function(id, cb){
+        orm.update(id, cb);
+    },
+    delete: function(id, cb){
+        orm.delete(id, cb);
+    }
+};
+
 
 //export burger.js
 module.exports = burger;
