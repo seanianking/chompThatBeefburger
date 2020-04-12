060bfd4dc55b017cd76d09842dee7ba24c17fb72
seanianking <seanianking@gmail.com>
2020-04-11 21:16:59 -0600

    created routes in controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,35 @@
 let express = require('express');
 let burger = require('../models/burger');
+const router = express.Router();
+
+router.get("/", function(req, res){
+    burger.selectAll(function(data){
+        let hbsObject = {burger: data};
+        res.render("index", hbsObject);
+    })
+});
+
+router.post("/api/add", function(req, res){
+    let name = req.body.name;
+    burger.create(name, data => {
+        res.json(data);
+    })
+})
+
+router.put("/api/eat/:id", function(req, res){
+    let id = req.params.id;
+
+    burger.update(id, data => {
+        res.json(data);
+    });
+});
+
+router.delete("/api/remove/:id", function(req, res){
+    let id = req.params.id;
+
+    burger.delete(id, data => {
+        res.json(data);
+    });
+});
 
 module.exports = router;
